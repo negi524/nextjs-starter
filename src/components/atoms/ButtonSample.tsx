287e268a8838c5cf77d8f143ca3613f5bac4b724
negi524 <egtnrumpbiizarre.info@gmail.com>
2023-02-25 17:23:44 +0900

    ファイル名修正 (#9)
    * rename filename
    
    * fix import name

--- src/components/atoms/ButtonSample.tsx
+++ src/components/atoms/ButtonSample.tsx
@@ -1,4 +1,4 @@
-import ThemeContext from "@/app/use-context-sample/theme-context";
+import ThemeContext from "@/app/use-context-sample/themeContext";
 import { useContext } from "react";
 
 export default function ButtonSample() {
