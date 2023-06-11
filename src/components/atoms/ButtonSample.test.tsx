4173a278b354239ad483b6f9a2b4bbebfe13bd32
negi524 <egtnrumpbiizarre.info@gmail.com>
2023-06-11 16:55:27 +0900

    コメント追加

--- src/components/atoms/ButtonSample.test.tsx
+++ src/components/atoms/ButtonSample.test.tsx
@@ -6,10 +6,12 @@ describe("ButtonSampleのテスト", () => {
   it("renders a heading", () => {
     render(<ButtonSample />);
 
+    // 見出し要素を名前を指定して取得
     const heading = screen.getByRole("heading", {
       name: "This is button sample",
     });
 
+    // 取得した要素がドキュメント内に存在したらテストOK
     expect(heading).toBeInTheDocument();
   });
 });
