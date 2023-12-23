8b14bd872879335b573ee322681e2a679869003f
negi524 <egtnrumpbiizarre.info@gmail.com>
2023-12-23 15:44:20 +0900

    フォーマット

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
-
 import { Button } from "react-bootstrap";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
