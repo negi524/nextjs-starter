1e63fa761698edb26763b3d70291be7a4d68c29c
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-02-23 14:40:58 +0900

    feat: CSRのページでも動作確認

--- src/app/use-context-sample/page.tsx
+++ src/app/use-context-sample/page.tsx
@@ -2,12 +2,19 @@
 
 import ButtonSample from "@/components/atoms/ButtonSample";
 import ThemeContext from "./themeContext";
+import stylex from "@stylexjs/stylex"
+
+const h1Style = stylex.create({
+  blueColorStyle: {
+    backgroundColor: 'gray'
+  },
+});
 
 export default function UseContextSamplePage() {
   return (
     <>
       <ThemeContext.Provider value={"dark"}>
-        <h1>useContext Sample Page</h1>
+        <h1 {...stylex.props(h1Style.blueColorStyle)}>useContext Sample Page</h1>
         <ButtonSample />
       </ThemeContext.Provider>
     </>
