50b23de16cd7d522e2d666583af9ba2a106de03b
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-03-10 12:38:52 +0900

    refactor: them contextをコメントアウト

--- src/components/atoms/ButtonSample.tsx
+++ src/components/atoms/ButtonSample.tsx
@@ -2,9 +2,12 @@ import ThemeContext from "@/app/use-context-sample/themeContext";
 import { useContext } from "react";
 
 export default function ButtonSample() {
+  // const theme = useContext(ThemeContext);
+
   return (
     <>
       <h3>This is button sample</h3>
+      {/* <p>theme is {theme}</p> */}
     </>
   );
 }
