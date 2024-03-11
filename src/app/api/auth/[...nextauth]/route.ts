07803e9238420d65010bcfc78dcbdbd0f1d61664
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-03-11 23:43:24 +0900

    feat: ログイン処理実装

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,64 @@
 // https://next-auth.js.org/getting-started/example
+import { randomBytes, randomUUID } from "crypto";
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
 import type { NextAuthOptions } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthOptions = {
   debug: true,
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID || "",
-      clientSecret: process.env.GITHUB_SECRET || "",
+    Credentials({
+      credentials: {
+        email: {
+          label: "メールアドレス",
+          type: "email",
+          placeholder: "メールアドレス",
+        },
+        password: {
+          label: "パスワード",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        const authResponse = await fetch(
+          `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.FIREBASE_API_KEY}`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(credentials),
+          },
+        );
+
+        if (!authResponse.ok) {
+          return null;
+        }
+
+        const user = await authResponse.json();
+
+        return user;
+      },
     }),
     // ...add more providers here
   ],
+  // secret
+  secret: process.env.NEXTAUTH_SECRET,
+  // jwt
+  jwt: {
+    maxAge: 3 * 24 * 60 * 60, // 3 days
+  },
+  /* session */
+  session: {
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+    updateAge: 24 * 60 * 60, // 24 hours
+    generateSessionToken: () => {
+      return randomUUID?.() ?? randomBytes(32).toString("hex");
+    },
+  },
 };
 
-export default NextAuth(authOptions);
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- src/app/login-page/page.tsx
+++ src/app/login-page/page.tsx
@@ -1,10 +1,27 @@
 "use client";
 import { useState } from "react";
 import { Container } from "react-bootstrap";
+import { signIn } from "next-auth/react";
 
 export default function LoginPage() {
   const [isLogin, setLogin] = useState<boolean>(false);
   const [email, setEmail] = useState<string>("");
+
+  async function formAction(formData: FormData) {
+    const result = await signIn("user", {
+      redirect: false,
+      email: formData.get("email"),
+      password: formData.get("password"),
+    });
+
+    if (result?.error) {
+      console.error("ログイン失敗");
+      setLogin(false);
+    } else {
+      console.log("ログイン成功");
+      setLogin(true);
+    }
+  }
 
   /**
    * ログイン状態のDOM
@@ -21,14 +38,14 @@ export default function LoginPage() {
    */
   const logoutView: JSX.Element = (
     <>
-      <p>未ログインです</p>
-      <form action="POST">
+      <form action={formAction}>
         <label htmlFor="email">メールアドレス</label>
         <input type="email" name="email" id="email" required />
         <label htmlFor="password">パスワード</label>
         <input type="password" name="password" id="password" required />
         <input type="submit" value="ログイン" />
       </form>
+      <p>未ログインです</p>
     </>
   );
   return (
