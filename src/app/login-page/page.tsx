7bf00f2aea91a4abfbf552bc530cfbd6de26d72f
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-03-11 22:48:51 +0900

    feat: ログイン項目を入力するフォーム作成

--- src/app/login-page/page.tsx
+++ src/app/login-page/page.tsx
@@ -3,12 +3,39 @@ import { useState } from "react";
 import { Container } from "react-bootstrap";
 
 export default function LoginPage() {
-  const [isLogin, setLogin] = useState(false);
+  const [isLogin, setLogin] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>("");
+
+  /**
+   * ログイン状態のDOM
+   */
+  const loginView: JSX.Element = (
+    <>
+      <p>ログイン中です</p>
+      <p>メールアドレス: {email}</p>
+    </>
+  );
+
+  /**
+   * 未ログイン状態のDOM
+   */
+  const logoutView: JSX.Element = (
+    <>
+      <p>未ログインです</p>
+      <form action="POST">
+        <label htmlFor="email">メールアドレス</label>
+        <input type="email" name="email" id="email" required />
+        <label htmlFor="password">パスワード</label>
+        <input type="password" name="password" id="password" required />
+        <input type="submit" value="ログイン" />
+      </form>
+    </>
+  );
   return (
     <>
       <Container>
         <h1>ログインページ</h1>
-        {isLogin ? <p>ログイン中です</p> : <p>未ログインです</p>}
+        {isLogin ? loginView : logoutView}
       </Container>
     </>
   );
