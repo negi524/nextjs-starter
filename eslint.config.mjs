730753c137b2853b9a9e6726c0e922f2192d34d7
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-04-24 22:51:01 +0900

    feat: pluginsとrulesを移動

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,18 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import stylex from "@stylexjs/eslint-plugin";
 
+// TODO: extends
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    plugins: {
+      stylex,
+    },
+    rules: {
+      "@stylexjs/valid-styles": "error",
+    },
+  },
 );
