aef1202516a4d93643e895972c7de616ca677648
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-04-30 17:37:44 +0900

    feat: FlatCompatを利用してcore-web-vitalsのextendsを実装

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,16 @@
 // @ts-check
 
+import { FlatCompat } from "@eslint/eslintrc";
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import stylex from "@stylexjs/eslint-plugin";
 
-// TODO: extends
+const compat = new FlatCompat();
+
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  ...compat.extends("next/core-web-vitals"),
   {
     plugins: {
       stylex,
