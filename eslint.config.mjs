2494e2f17d7994b95ee522c48540214ceea2bb4c
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-05-05 10:52:04 +0900

    feat: ベースの設定追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,31 @@
 // @ts-check
 
-import { FlatCompat } from "@eslint/eslintrc";
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
-import stylex from "@stylexjs/eslint-plugin";
+import globals from "globals";
+// import { FlatCompat } from "@eslint/eslintrc";
+// import stylex from "@stylexjs/eslint-plugin";
 
-const compat = new FlatCompat();
+// const compat = new FlatCompat();
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...compat.extends("next/core-web-vitals"),
+// console.log(tseslint.configs.recommended, { depth: 3 });
+const config = tseslint.config(
   {
-    plugins: {
-      stylex,
-    },
-    rules: {
-      "@stylexjs/valid-styles": "error",
+    name: "global-setting",
+    ignores: ["out/", ".next/", ".babelrc.js", "next.config.js"],
+  },
+  eslint.configs.recommended,
+  // ...tseslint.configs.recommended,
+  // ...tseslint.configs.stylistic,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
     },
   },
 );
+
+console.dir(config, { depth: 4 });
+
+export default config;
